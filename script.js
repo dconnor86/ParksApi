8774992bde7e5a2cd86589013e5800c52e83ed21
dconnor86 <dconnor86@gmail.com>
2019-04-03 21:12:08 -0400

    finished product working

---
+++ script.js
@@ -0,0 +1,33 @@
+
+function queryResults(){
+let state = document.getElementById('stateSearch').value;
+URL = 'https://developer.nps.gov/api/v1/parks?q='+state+'&api_key=hppPR6XUS6aQatRAXO2X5QPSNTdeLSthZuwa6Esr';
+fetch(URL)
+.then(response => response.json())
+.then(responseJson => console.log(results(responseJson)))
+.catch(error => alert('ERROR!'));
+};
+
+function results(responseJson){
+    for(let i=0; i<responseJson.data.length & i<($('#js-max-results').val()); i++){
+        responseJson.data[i].name;
+        responseJson.data[i].description;
+        responseJson.data[i].url;
+        $('.parks').append(
+           `<h1>${responseJson.data[i].name}</h1><br>`,
+           `<p>${responseJson.data[i].description}</p>`,
+           `<a href= ${responseJson.data[i].url}> Link </a>`
+        )}
+
+}
+
+function renderSubmit(){
+    $('form').on('submit', function (event){
+    (event).preventDefault();
+    $('.parks').empty();
+    queryResults();
+})
+};
+
+renderSubmit();
+
